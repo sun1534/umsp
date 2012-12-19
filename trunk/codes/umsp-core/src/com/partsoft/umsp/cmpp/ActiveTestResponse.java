c0bc0cca066fb2fe507a24b86712e4853988bd81
neeker@gmail.com <neeker@gmail.com@5b6f9bff-9d5e-deca-1e0b-3eff068ab56c>
2012-12-19 09:58:36 +0000

    修改ActiveResponse少应答一个字节的错误

--- trunk/codes/umsp-core/src/com/partsoft/umsp/cmpp/ActiveTestResponse.java
+++ trunk/codes/umsp-core/src/com/partsoft/umsp/cmpp/ActiveTestResponse.java
@@ -1,9 +1,13 @@
 package com.partsoft.umsp.cmpp;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 import com.partsoft.umsp.cmpp.Constants.Commands;
 
 public class ActiveTestResponse extends CmppDataPacket {
-
+	
 	protected ActiveTestResponse() {
 		super(Commands.CMPP_ACTIVE_TEST_RESP);
 	}
@@ -11,6 +15,23 @@ public class ActiveTestResponse extends CmppDataPacket {
 	@Override
 	public ActiveTestResponse clone() {
 		return (ActiveTestResponse) super.clone();
+	}
+	
+	@Override
+	protected void writeDataOutput(DataOutput out) throws IOException {
+		super.writeDataOutput(out);
+		out.writeByte(0);
+	}
+	
+	@Override
+	protected void readDataInput(DataInput in) throws IOException {
+		super.readDataInput(in);
+		in.readByte();
+	}
+	
+	@Override
+	public int getDataSize() {
+		return super.getDataSize() + 1;
 	}
 	
 }
